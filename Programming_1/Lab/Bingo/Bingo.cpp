e9909bbff6397350b2f5118dfb877a5c4d542e54
kaucrow <kaucrow@protonmail.com>
2023-07-29 08:06:05 -0400

    Marked card now gets marked and updated

--- Programming_1/Lab/Bingo/Bingo.cpp
+++ Programming_1/Lab/Bingo/Bingo.cpp
@@ -4,10 +4,11 @@
 #include <iomanip>
 using std::cout, std::endl, std::setw, std::fill_n;
 
+void draw(int player[], int marked[]);
+
 int main(){
     //      B       I       N       G       O
     //    1-15    16-30   31-45   46-60   61-75    < -- number range for each row
-
     // SET UP VARIABLES, ARRAYS, AND RAND() SEED
     srand(time(NULL));
     int currBall, currRow, currCol;
@@ -15,14 +16,14 @@ int main(){
     bool markedCard[5][5] = {};
     char title[5] = {'B','I','N','G','O'};
     bool usedNum[75] = {};   // array to keep track of already used numbers/cards
-    //fill_n(usedNum, 75, false);
- 
+    
     // PRINT "BINGO"
     cout << setw(3) << title[0];
     for(int i = 1; i < 5; i++){
         cout << setw(4) << title[i];
     }
     cout << endl;
+    
     // ASSIGN PLAYER CARD NUMBERS
     for(int i = 0; i < 5; i++){
         for(int j = 0; j < 5; j++){
@@ -48,14 +49,16 @@ int main(){
         }while(usedNum[currBall - 1] == true);
         usedNum[currBall - 1] = true;
         markedCard[currRow][currCol] = 1;
-    }
-    for(int i = 0; i < 5; i++){
+       
         for(int j = 0; j < 5; j++){
-            cout << markedCard[i][j] << " ";
+            for(int k = 0; k < 5; k++){
+                cout << markedCard[j][k] << " ";
+            }
+            cout << endl;
         }
-        cout << endl;
+        getchar();      // wait until user presses a key
     }
-
+    cout << "end\n";
     /*for(int i = 0; i < 75; i++){
         cout << usedNum[i];
     }*/
