eb9c4c0c914b0e53b4bebe0f4fe66fa623911400
kaucrow <kaucrow@protonmail.com>
2023-07-29 21:56:46 -0400

    Optimization fixes

--- Programming_1/Lab/Bingo/Bingo.cpp
+++ Programming_1/Lab/Bingo/Bingo.cpp
@@ -3,8 +3,8 @@
 #include <time.h>
 #include <iomanip>
 #include <algorithm>
-using   std::cout, std::endl, std::setw, std::fill_n,
-        std::any_of, std::begin, std::end;
+using   std::cout, std::endl, std::setw, std::any_of, 
+        std::begin, std::end;
 
 void DrawTables(int playerCard[][5], bool markedCard[][5]);
 void ClrScr();
@@ -20,7 +20,8 @@ int main(){
     bool currSkip;
     int playerCard[5][5] = {};
     bool markedCard[5][5] = {}; markedCard[2][2] = 1;
-    bool usedNum[75] = {};                                  // to keep track of already used numbers/balls
+    bool usedNumPlayer[75] = {};                            // to keep track of already used numbers in player card
+    bool usedNumBall[75] = {};                              // to keep track of already used balls
     int countCol[5] = {0, 0, 1, 0, 0};                      // to keep the sum of the '1's in each column
     int countRow[5] = {0, 0, 1, 0, 0};                      // to keep the sum of the '1's in each row
     int countDiagP = 0, countDiagS = 0;
@@ -31,8 +32,8 @@ int main(){
             do{
                 // based on the current row, set a number in the row range
                 playerCard[i][j] = ((rand() % 15)) + (1) + (15*j); 
-            }while(usedNum[playerCard[i][j] - 1]);
-            usedNum[playerCard[i][j] - 1] = true;           // set the current number as used
+            }while(usedNumPlayer[playerCard[i][j] - 1]);
+            usedNumPlayer[playerCard[i][j] - 1] = true;     // set the current number as used
         }
     }
     playerCard[2][2] = 0;
@@ -53,7 +54,6 @@ int main(){
     getchar();
     cout.flush();
     ClrScr();
-    fill_n(usedNum, 75, false);
     for(int i = 1; i <= 71; i++){ 
         currSkip = 1;
         
@@ -61,29 +61,32 @@ int main(){
         do{
             // keep generating the ball until the current ball hasn't been used yet
             currBall = (rand() % 75) + 1;
-        }while(usedNum[currBall - 1]);
-        usedNum[currBall - 1] = 1;                          // set the ball as used
+        }while(usedNumBall[currBall - 1]);
+        usedNumBall[currBall - 1] = 1;                      // set the ball as used
         
         // check if the ball is in the player card
-        for(int j = 0; j < 5; j++){
-            for(int k = 0; k < 5; k++){
-                if(playerCard[j][k] == currBall){
-                    markedCard[j][k] = 1;
-                    countRow[j] += 1; countCol[k] += 1;
-                    if(j == k) countDiagP++;                // element in 1st Diagonal: if currRow == currCol
-                    if((5 - 1) == (j + k)) countDiagS++;    // element in 2nd Diagonal: if number of col - 1 == currRow + currCol
-                    // check if the player won
-                    if( any_of(begin(countRow), end(countRow), [](int n){return n == 5;}) ||
-                        any_of(begin(countCol), end(countCol), [](int n){return n == 5;}) ||
-                        countDiagP == 4 || countDiagS == 4){
-                        DrawTables(playerCard, markedCard);
-                        cout << setw(24) << "BINGO! :)\n";
-                        cout << setw(24) << "Turnos totales: " << i << endl;
-                        return 0;
+        if(usedNumPlayer[(currBall - 1)]){
+            // if it is, find its position in the player card
+            for(int j = 0; j < 5; j++){
+                for(int k = 0; k < 5; k++){
+                    if(playerCard[j][k] == currBall){
+                        markedCard[j][k] = 1;
+                        countRow[j] += 1; countCol[k] += 1;
+                        if(j == k) countDiagP++;                // element in 1st Diagonal: if currRow == currCol
+                        if((5 - 1) == (j + k)) countDiagS++;    // element in 2nd Diagonal: if number of col - 1 == currRow + currCol
+                        // check if the player won
+                        if( any_of(begin(countRow), end(countRow), [](int n){return n == 5;}) ||
+                            any_of(begin(countCol), end(countCol), [](int n){return n == 5;}) ||
+                            countDiagP == 4 || countDiagS == 4){
+                            DrawTables(playerCard, markedCard);
+                            cout << setw(24) << "BINGO! :)\n";
+                            cout << setw(24) << "Turnos totales: " << i << endl;
+                            return 0;
+                        }
+                        currSkip = 0;                           // when currSkip is 0, the draw phase won't be skipped
+                        j = 5;                                  // stop the player card search after the pos has been found
+                        break;
                     }
-                    currSkip = 0;                           // when currSkip is 0, the draw phase won't be skipped
-                    j = 5;                                  // stop the player card search after the pos has been found
-                    break;
                 }
             }
         }
