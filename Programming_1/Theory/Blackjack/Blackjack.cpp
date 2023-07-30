a03df8a5f56c65d513329831887118ee6379c2f9
kaucrow <kaucrow@protonmail.com>
2023-07-30 18:43:37 -0400

    The player can "switch" between hands, but do nothing with them

--- Programming_1/Theory/Blackjack/Blackjack.cpp
+++ Programming_1/Theory/Blackjack/Blackjack.cpp
@@ -1,24 +1,28 @@
 #include <iostream>
+#include <iomanip>
 #include <stdlib.h>
 #include <time.h>
 #include <limits>
+#include <conio.h>
 
 using   std::cout, std::cin, std::endl, std::copy,
-        std::begin, std::end;
+        std::begin, std::end, std::setw;
 
-int DrawCard(int deckCards[], int deckNew[], int &cardCount);
+int GetCard(int deckCards[], int deckNew[], int &cardCount);
+void DrawHands(int playerHands[]);
 void ClrScr();
 
 int main(){
-    int cardsSum[4] = {};
+    int playerHands[4] = {};                       // sum of the cards in each of the player's hands. One element for each hand
     int deckCards[52] = {};
     const int deckNew[52] = {   1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,
                                 9,9,9,9,10,10,10,10,11,11,11,11,12,12,12,12,13,13,13,13 };
-    int nextCard, cardCount = 52, placedCardCount = 1, playerMoney = 100, minBet, playerBet;
+    int nextCard, cardCount = 52, availHands = 4, placedCardCount = 1, playerMoney = 100,
+        minBet, playerBet, dealerSum = 0, handSelect = 0, currKey;
     char flagSplit;
     
     srand(time(NULL));
-    copy(deckNew, deckNew + 52, deckCards);         // get a new deck
+    copy(deckNew, deckNew + 52, deckCards);     // get a new deck
     
     /*cout << "Cuanto dinero tiene?: ";
     cin >> playerMoney;
@@ -29,50 +33,85 @@ int main(){
     */
     while(playerMoney > 0){
         ClrScr();
-        cardsSum[0] = (rand()%3+1);     //DrawCard(deckCards, deckNew, cardCount);
      
         // *** SET UP THE STARTING HAND(s) ***
+        playerHands[0] = (rand()%1+1);                                 // replace by: GetCard(deckCards, deckNew, cardCount);
         for(int splitCount = 0; splitCount <= 3; splitCount++){
-            cout << "outer iteration: " << splitCount << endl;
-            for(int i = 0; i < 4; i++) cout << cardsSum[i] << " ";
-            cout << endl;
+            // the value of temp is a displacement added to the loop 
+            // for avoiding putting more than two cards on any hand
             for(int temp = (placedCardCount - splitCount - 1); temp < 4; temp++){
-                if(!cardsSum[temp]){ splitCount = 4; break; }
-                nextCard = (rand()%3)+1;
+                // if a 0 is reached in the playerHands, stop the setup
+                if(!playerHands[temp]){ availHands = splitCount + 1; splitCount = 4; break; }
+                nextCard = (rand() % 1) + 1;
                 placedCardCount++;
-                cout << "nextCard: " << nextCard << endl;
-                if(cardsSum[temp] == nextCard){
+                cout << "Salio un: " << nextCard << endl;
+                // executes if the card on the current hand is equal to the drawn card
+                if(playerHands[temp] == nextCard && splitCount < 3){
                     do{
                         cout << "Desea dividir? (y/n): ";
                         flagSplit = getchar();
                         // to remove the "\n" left after getchar()
                         cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); 
                         cout << "flagSplit: " << flagSplit << endl;
-                    }while(flagSplit != 'y' && flagSplit != 'n'); 
-                    if(flagSplit == 'y'){ cardsSum[splitCount+1] = nextCard; break; }
+                    }while(flagSplit != 'y' && flagSplit != 'n');
+                    // if the player wants to split, put the drawn card in the nearest empty
+                    // playerHands[] element and exit this loop to increase the splitCount
+                    if(flagSplit == 'y'){ playerHands[splitCount+1] = nextCard; break; }
                 }
-                cardsSum[temp] += nextCard;
-                cout << "inner iteration: " << temp << endl;
-                for(int i = 0; i < 4; i++) cout << cardsSum[i] << " ";
-                cout << endl;
+                // executes if the player can't split or doesn't want to
+                playerHands[temp] += nextCard;    
             }
         }
-        cout << "\nEXIT\n";
+        cout << "\nEXIT\n";                                         // marker
+        cout << "HANDS: " << availHands << endl;
+        // *** PRINT THE CURRENT CARDS SUM ***
+        DrawHands(playerHands);
+        
+        // *** USER INPUT ***
+        // [<- : 75] [-> : 77] [Enter : 13]
+        do{
+            if(handSelect != 99){
+                DrawHands(playerHands);
+                cout << setw((handSelect*4) + 4) << '*' << endl;
+            }
+            cout << "Cual mano? (<-/->/Enter): ";
+            do{
+                currKey = getch();
+                //cout << "\ncurrKey: " << currKey;
+            }while(currKey != 75 && currKey != 77 && currKey != 13);
+            cout << '\n' << currKey;
+            switch(currKey)
+            {
+                case 75:
+                    if(handSelect > 0) handSelect--;
+                    cout << "handSelect: " << handSelect << endl;
+                    cout << " 75A";
+                    break;
+                case 77:
+                    if(handSelect < availHands - 1) handSelect++;
+                    cout << "handSelect: " << handSelect << endl;
+                    cout << " 77B";
+                    break;
+                case 13:
+                    cout << " EnterC";
+                    cout << "handSelect: " << handSelect << endl;
+                    handSelect = 99;
+                    break;
+            }
+        cout << "\n*** RESET ***\n";
+        }while(availHands > 0);
 
-        // *** PRINT THE CURRENT CARDS SUM
-        for(int i = 0; i < 4; i++) cout << cardsSum[i] << " ";
-        // cout << "amount: " << cardCount << endl;
         getchar();
         cout << "\n*** RESET ***\n";
-        for(int i = 0; i < 4; i++) cardsSum[i] = 0;
+        for(int i = 0; i < 4; i++) playerHands[i] = 0;
         placedCardCount = 1;
         playerMoney--;
     }
 }
 
-//  *** DRAW A CARD FROM THE CURRENT DECK AND GET A NEW ***
+//  *** GET A CARD FROM THE CURRENT DECK AND GET A NEW  ***
 //  *** DECK IF THE CURRENT ONE IS EMPTY                ***
-int DrawCard(int deckCards[], int deckNew[], int &cardCount){
+int GetCard(int deckCards[], int deckNew[], int &cardCount){
     int pos, drawnCard;
     
     // check if the deck is empty, and if so, grab a new one
@@ -88,6 +127,11 @@ int DrawCard(int deckCards[], int deckNew[], int &cardCount){
     return drawnCard;
 }
 
+void DrawHands(int playerHands[]){
+    cout << " *** PLAYER HANDS ***\n";
+    for(int i = 0; i < 4; i++){ if(!playerHands[i]) break; cout << setw(4) << playerHands[i]; }
+    cout << endl;
+}
 void ClrScr(){
     #ifdef _WIN32
         // if on Windows OS
