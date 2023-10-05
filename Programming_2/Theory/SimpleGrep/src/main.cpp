23265d20825b450a94eec869ebdb20498af2e97b
kaucrow <kaucrow@protonmail.com>
2023-10-04 20:02:40 -0400

    feat: Made Makefile Windows/Linux compatible

--- Programming_2/Theory/SimpleGrep/src/main.cpp
+++ Programming_2/Theory/SimpleGrep/src/main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 #include <fstream>
-using std::cout, std::cerr, std::cin, std::getline, std::ifstream;
+using std::cout, std::cerr, std::cin, std::string, std::getline, std::ifstream;
 
 int main(int argc, char* argv[]){
-    if(argc < 3){
+    if(argc == 1){
         cerr << "Usage: sgrep [OPTION] PATTERN [FILE]\n";
         return 1;
+    }
+    else if(argc == 2){
+        string readed;
+        while(getline(cin, readed)){ 
+            cout << readed;
+        }
     }
 
     ifstream readFile(argv[argc - 1]);
@@ -13,4 +19,5 @@ int main(int argc, char* argv[]){
         cerr << "sgrep: [ ERR ] FILE \"" << argv[argc - 1] << "\" DOES NOT EXIST\n";
         return 1;
     }
+
 }
