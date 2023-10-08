8be64f31a7b84d36aa63625c15494084ce4f11ad
kaucrow <kaucrow@protonmail.com>
2023-10-07 21:43:44 -0400

    docs: Commented code

--- Programming_2/Theory/Blockbuster/src/main.cpp
+++ Programming_2/Theory/Blockbuster/src/main.cpp
@@ -6,22 +6,22 @@
 using   std::cout, std::cerr, std::cin, std::string, std::getline,
         std::wifstream, std::wifstream, std::wstring, std::wcout;
 
-int GetNumMovies(wifstream& inFile);
-void PopulateMovieList(Movie movieList[], wifstream& inFile);
+int GetNumMovies(wifstream& inFile);            // returns the num of movies in the movies.csv file
+void PopulateMovieList(Movie movieList[], wifstream& inFile);   // puts the movies in the movies.csv file into the movie list
 int main(){
-    boost::locale::generator gen;
-    std::locale loc = gen("en_US");
-    std::locale::global(loc);
-    _setmode(_fileno(stdout), _O_U8TEXT);
+    boost::locale::generator gen;               // create the locale generator.
+    std::locale loc = gen("en_US");             // create an "en_US" locale
+    std::locale::global(loc);                   // and set it as the global locale.
+    _setmode(_fileno(stdout), _O_U8TEXT);       // change the STDOUT mode to use UTF-8 characters.
 
-    string inFileName = "./data/movies.csv";
+    string inFileName = "./data/movies.csv";    
     wifstream inFile(inFileName);
-    inFile.imbue(loc);
+    inFile.imbue(loc);                          // apply the locale to the movies.csv file.
     if(!inFile){ cerr << "ERR: FILE \"" << inFileName << "\" COULD NOT BE OPENED."; return 1; }
 
-    Movie movieList[GetNumMovies(inFile) + 3001];
-    wcout << GetNumMovies(inFile) << '\n';
-    wcout << sizeof(movieList) << '\n';
+    Movie movieList[GetNumMovies(inFile) + 3001];       // create a list of movies for 4000 movies, where the first index is unused
+    wcout << GetNumMovies(inFile) << '\n';      // debug
+    wcout << sizeof(movieList) << '\n';         // debug
     //PopulateMovieList(movieList, inFile);
 }
 
@@ -61,23 +61,25 @@ int main(){
 }*/
 
 int GetNumMovies(wifstream& inFile){
-    inFile.seekg(0, std::ios_base::end);        // end:  74182
-    std::wifstream::pos_type pos = inFile.tellg();
+    inFile.seekg(0, std::ios_base::end);            // move to the EOF
+    std::wifstream::pos_type pos = inFile.tellg();  // get the curr pos and assign to a variable
     pos = int(pos) - 1;
-    inFile.seekg(pos);
-    std::wcout << pos << '\n';
+    inFile.seekg(pos);              // go back one char
+    std::wcout << pos << '\n';      // debug
 
     wchar_t ch;
-    while(ch != L'\n'){
+    // executes while a newline isn't found
+    while(ch != '\n'){
         pos = int(pos) - 1;
-        inFile.seekg(pos);
-        inFile.get(ch);
-        std::wcout << ch << ' ';
+        inFile.seekg(pos);          // go back one char
+        inFile.get(ch);             // get the curr char
+        std::wcout << ch << ' ';    // debug
     }
+
     wstring lastLine;
     getline(inFile, lastLine);
-    wcout << lastLine << '\n';
+    wcout << lastLine << '\n';      // debug
 
-    inFile.seekg(0, std::ios_base::beg);
-    return stoi(lastLine);
+    inFile.seekg(0, std::ios_base::beg);    // put the inFile position back to the beginning
+    return stoi(lastLine);          // return the first number in the last line
 }
