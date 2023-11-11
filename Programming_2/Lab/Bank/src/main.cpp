5ed4f252c60f9571c0b91ad0fbb0bab747345c82
kaucrow <121270913+Kaucrow@users.noreply.github.com>
2023-11-11 11:25:30 -0400

    feat: Added basic UI

--- Programming_2/Lab/Bank/src/main.cpp
+++ Programming_2/Lab/Bank/src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <limits>
 #include <iomanip>
 #include <cassert>
 //#include <fcntl.h>              // For _setmode().
@@ -12,6 +13,13 @@
 
 using   std::cout, std::cerr, std::cin, std::string, std::getline,
         std::ofstream, std::ifstream;
+
+enum { SEARCH = 1, EXIT = 2 };      // Actions.
+
+/**
+ * @brief OS agnostic clear screen function.
+ */
+void ClrScr();
 
 int main(){
     /*boost::locale::generator gen;               // Create the locale generator.
@@ -45,4 +53,95 @@ int main(){
     MergeSort(ciList, 1, totalClients);
     MergeSort(accNumList, 1, totalClients);
     MergeSort(nameList, 1, totalClients);
+
+    int action;
+    string username;
+    while(true){
+        // Main menu screen. //
+        ClrScr();
+        cout    << "*** MENU ***\n"
+                << "(1) Login\n"
+                << "(2) Exit\n"
+                << "Select option: ";
+        //cin >> action;
+        switch(action){
+            case 1: break;
+            case 2:
+                cout << "\nTerminating execution...\n";
+                return 0;
+        }
+        //cin.ignore(1);
+
+        // Login screen. //
+        ClrScr();
+        cout    << "*** LOGIN ***\n"
+                << "-> User: ";
+        //getline(cin, username);
+
+        while(true){
+            // Main actions screen. //
+            ClrScr();
+            cout    << "*** CHOOSE AN ACTION ***\n"
+                    << "(1) Search for a client\n"
+                    << "(2) Exit\n"
+                    << "Select option: ";
+
+            cin >> action;
+            while(action < SEARCH || action > EXIT){
+                cout << "INVALID OPTION.\nSelect option: ";
+                cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                cin >> action;
+            }
+            cin.ignore(1);
+
+            if(action == SEARCH){
+                ClrScr();
+                cout    << "*** CLIENT SEARCH ***\n"
+                        << "(1) Search by CI\n"
+                        << "(2) Search by account number\n"
+                        << "(3) Search by client name\n"
+                        << "Select option: ";
+
+                cin >> action;
+                while(action < 1 || action > 3){
+                    cout << "INVALID OPTION.\nSelect option: ";
+                    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                    cin >> action;
+                }
+                cin.ignore(1);
+
+                int intSearch;
+                long long int llintSearch;
+                string strSearch;
+                ClrScr();
+                switch(action){
+                    case 1:
+                        cout << "CI to search for: ";
+                        cin >> intSearch;
+                        cin.ignore(1);
+                        break;
+                    case 2:
+                        cout << "Account number to search for: ";
+                        cin >> llintSearch;
+                        cin.ignore(1);
+                        break;
+                    case 3:
+                        cout << "Client name to search for: ";
+                        getline(cin, strSearch);
+                        break;
+                } 
+            }
+            else break;
+        }
+    }
+}
+
+void ClrScr(){
+    #ifdef _WIN32
+        // If on Windows OS
+        std::system("cls");
+    #else
+        // Assuming POSIX OS
+        std::system("clear");
+    #endif
 }
