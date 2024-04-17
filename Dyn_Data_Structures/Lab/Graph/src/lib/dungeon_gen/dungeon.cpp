fbe0ccdd3f20f7d42a73a9b298b93d31c2ace716
kaucrow <kaucrow@protonmail.com>
2024-04-16 22:39:44 -0400

    refactor: The connections are now bidirectional

--- Dyn_Data_Structures/Lab/Graph/src/lib/dungeon_gen/dungeon.cpp
+++ Dyn_Data_Structures/Lab/Graph/src/lib/dungeon_gen/dungeon.cpp
@@ -35,12 +35,25 @@ string gen_level_layout_file(DungeonProperties &prop) {
 
     int filenum = 1;
 
-    for (const auto &entry : fs::directory_iterator(data_path))
-        filenum = get_file_number(entry.path().filename().generic_string()) + 1;
+    for (const auto &entry : fs::directory_iterator(data_path)) {
+        int new_filenum = get_file_number(entry.path().filename().generic_string()) + 1;
+        if (new_filenum > filenum)
+            filenum = new_filenum;
+    }
 
     string filename = "level_" + std::to_string(filenum) + ".txt";
 
     ofstream out("./data/" + filename);
+
+    int start_room = Random(1, prop.room_amt);
+
+    int end_room = Random(1, prop.room_amt);
+    while (start_room == end_room)
+        end_room = Random(1, prop.room_amt);
+
+    int key_room = Random(1, prop.room_amt);
+    while (key_room == start_room || key_room == end_room)
+        key_room = Random(1, prop.room_amt);
 
     if (!out)
         panic("[ ERR ] Couldn't create the output file.");
@@ -50,41 +63,36 @@ string gen_level_layout_file(DungeonProperties &prop) {
 
     LinkedList<LinkedList<int>> connections;
 
-    for (int i = 1; i <= prop.room_amt; i++) {
+    for (int i = 1; i <= prop.room_amt; i++)
         connections.append(LinkedList<int>());
 
+    for (int i = 1; i <= prop.room_amt; i++) {
         for (int j = 1; j <= Random(prop.min_conn, prop.max_conn); j++) {
             cout << i << '\n';
             int room_to = Random(1, prop.room_amt);
 
-            while (room_to == i || connections[i - 1].find(room_to) != -1)
+            while 
+            (
+                room_to == i ||
+                (i == start_room && room_to == end_room) ||
+                (i == end_room && room_to == start_room)
+            )
                 room_to = Random(1, prop.room_amt);
 
-            connections[i - 1].append(room_to);
+            if (connections[i - 1].find(room_to) == -1) {
+                connections[i - 1].append(room_to);
+                connections[room_to - 1].append(i);
 
-            out << i << ',' << room_to << ',' << Random(1, 9) << '\n';
+                int weight = Random(1, 9);
+                out << i << ',' << room_to << ',' << weight << '\n';
+                out << room_to << ',' << i << ',' << weight << '\n';
+            }
         }
     }
 
     out << "\n--- ROOM PROPERTIES ---\n"
-        << "room,property\n";
-
-    int start_room = Random(1, prop.room_amt);
-
-    int end_room = Random(1, prop.room_amt);
-    while 
-    (
-        start_room == end_room ||
-        connections[start_room - 1].find(end_room) != -1 ||
-        connections[end_room - 1].find(start_room) != -1
-    )
-        end_room = Random(1, prop.room_amt);
-
-    int key_room = Random(1, prop.room_amt);
-    while (key_room == start_room || key_room == end_room)
-        key_room = Random(1, prop.room_amt);
-
-    out << start_room << ",IS_START\n"
+        << "room,property\n"
+        << start_room << ",IS_START\n"
         << end_room << ",IS_END\n"
         << key_room << ",IS_KEYROOM\n";
 
@@ -95,5 +103,6 @@ int get_file_number(string filename) {
     cout << filename << '\n';
     size_t first_pos = filename.find_first_of("0123456789");
     size_t last_pos = filename.find_last_of("0123456789");
-    return stoi(filename.substr(first_pos, first_pos - last_pos + 1));
+    cout << filename.substr(first_pos, last_pos - first_pos + 1) << '\n';
+    return stoi(filename.substr(first_pos, last_pos - first_pos + 1));
 }
